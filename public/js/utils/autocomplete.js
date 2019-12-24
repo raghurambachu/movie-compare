b8a1606570d35f7d4c3b6179dcbe63c812fcff61
Raghuram Bachu <1993raghuram@gmail.com>
2019-12-24 10:32:23 +0530

    Fixed issue related to autocomplete dropdown, if user clicks outside dropdown, the dropdown collapses.

--- public/js/utils/autocomplete.js
+++ public/js/utils/autocomplete.js
@@ -1,6 +1,5 @@
-const createAutoComplete =  ({root,fetchData,displayList,fetchDetails})=>{
-
-        root.innerHTML = `
+const createAutoComplete = ({ root, fetchData, displayList, fetchDetails }) => {
+	root.innerHTML = `
         <div class="dropdown">
                         <div class="control">
                                 <label class="label">Search Movie</label>       
@@ -12,47 +11,38 @@ const createAutoComplete =  ({root,fetchData,displayList,fetchDetails})=>{
                         </div>
                 </div>
         </div>
-        `
+        `;
 
-        const inputDOM = root.querySelector("input");
-        const resultsDOM = root.querySelector(".results");
-        const dropdownDOM = root.querySelector('.dropdown')
+	const inputDOM = root.querySelector('input');
+	const resultsDOM = root.querySelector('.results');
+	const dropdownDOM = root.querySelector('.dropdown');
 
+	const listOfItems = (items) => {
+		inputDOM.setAttribute('placeholder', 'search movie');
+		dropdownDOM.classList.add('is-active');
+		resultsDOM.innerHTML = '';
+		if (!items.length) {
+			dropdownDOM.classList.remove('is-active');
+			inputDOM.setAttribute('placeholder', 'not found.');
+		} else {
+			for (let item of items) {
+				displayList(item, resultsDOM);
+			}
+			const options = resultsDOM.querySelectorAll('a div');
+			options.forEach((option) => {
+				option.addEventListener('click', () => {
+					dropdownDOM.classList.remove('is-active');
+					inputDOM.value = option.innerText;
+					fetchDetails(option);
+				});
+			});
+			document.addEventListener('click', (event) => {
+				if (!dropdownDOM.contains(event.target)) {
+					dropdownDOM.classList.remove('is-active');
+				}
+			});
+		}
+	};
 
-        const listOfItems = (items)=>{
-                inputDOM.setAttribute("placeholder","search movie")
-                dropdownDOM.classList.add("is-active")
-                resultsDOM.innerHTML =""
-                if(!items.length){
-                        dropdownDOM.classList.remove("is-active");
-                        inputDOM.setAttribute("placeholder","not found.")
-                }else{
-                        for(let item of items){
-                                displayList(item,resultsDOM)
-                        }
-                        const options = resultsDOM.querySelectorAll("a div")
-                        options.forEach(option => {
-                                option.addEventListener("click",()=>{
-                                        dropdownDOM.classList.remove("is-active");
-                                        inputDOM.value = option.innerText;
-                                        fetchDetails(option)
-                                        
-                                })
-                        });
-
-                }
-               
-               
-        }
-
-        fetchData(inputDOM,listOfItems);
-
-       
-
-       
-        
-       
-
-
-        
-}
+	fetchData(inputDOM, listOfItems);
+};
